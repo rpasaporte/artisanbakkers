e992fcbc271342fd14f85e32d0845384c5da99dc
Rye Pasaporte <ryepassport@gmail.com>
2025-03-25 21:40:15 +0100

    update link
    Signed-off-by: Rye Pasaporte <ryepassport@gmail.com>

--- src/app/proofing/page.tsx
+++ src/app/proofing/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
+import logoImg from '../../../public/b-blue.png';
 
 export default function Proofing() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-white text-black">
-      <Image src={'/b-blue.png'} alt="Coming Soon" width={160} height={160} className="mb-4" />
+      <Image src={logoImg} alt="Coming Soon" width={160} height={160} className="mb-4" />
       <h1 className="text-5xl font-bold">Coming Soon</h1>
     </div>
   );
