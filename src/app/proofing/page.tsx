beff5b77d29622821f859d955ac07d07d3da74b5
Rye Pasaporte <ryepassport@gmail.com>
2025-03-25 21:35:12 +0100

    fix link

--- src/app/proofing/page.tsx
+++ src/app/proofing/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Proofing() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-white text-black">
-      <Image src="/b-blue.png" alt="Coming Soon" width={160} height={160} className="mb-4" />
+      <Image src={'/b-blue.png'} alt="Coming Soon" width={160} height={160} className="mb-4" />
       <h1 className="text-5xl font-bold">Coming Soon</h1>
     </div>
   );
